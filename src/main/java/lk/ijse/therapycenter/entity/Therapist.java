c506352082509b92b9db6e8c568fe749f7b821d1
Oshan <git config --global user.email>
2026-05-10 16:40:09 +0530

    create therapist entity

--- src/main/java/lk/ijse/therapycenter/entity/Therapist.java
+++ src/main/java/lk/ijse/therapycenter/entity/Therapist.java
@@ -1,4 +1,28 @@
 package lk.ijse.therapycenter.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Therapist {
+
+    @Id
+    private String therapistId;
+
+    private String name;
+    private String specialization;
+    private String availability;
+
+    @OneToMany(mappedBy = "therapist", cascade = CascadeType.ALL)
+
+    private List<TherapySession> sessions;
 }
