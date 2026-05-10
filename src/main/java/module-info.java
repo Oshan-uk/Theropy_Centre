8951350bae9166e183333cd12162700f2dc7e484
Oshan <git config --global user.email>
2026-05-10 10:52:22 +0530

    create factory configuration class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module TherapyCenter {
     requires jakarta.persistence;
     requires static lombok;
+    requires org.hibernate.orm.core;
+    requires java.naming;
+
+    opens lk.ijse.therapycenter.entity to org.hibernate.orm.core;
 }
