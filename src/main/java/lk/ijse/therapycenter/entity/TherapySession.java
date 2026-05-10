779252df8b9e0ab179f5eef08af9a02a95fb9893
Oshan <git config --global user.email>
2026-05-10 16:44:12 +0530

    create therapy session entity

--- src/main/java/lk/ijse/therapycenter/entity/TherapySession.java
+++ src/main/java/lk/ijse/therapycenter/entity/TherapySession.java
@@ -1,4 +1,33 @@
 package lk.ijse.therapycenter.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class TherapySession {
+
+    @Id
+    private String sessionId;
+
+    private String sessionDate;
+    private String sessionTime;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(name = "therapist_id")
+    private Therapist therapist;
+
+    @ManyToOne
+    @JoinColumn(name = "program_id")
+    private TherapyProgram program;
 }
