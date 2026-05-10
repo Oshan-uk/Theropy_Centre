8b2ce7f4cc2e87675e61a56ad8d2c5b869f5cb29
Oshan <git config --global user.email>
2026-05-10 17:49:22 +0530

    create app initializer

--- src/main/java/lk/ijse/therapycenter/AppInitializer.java
+++ src/main/java/lk/ijse/therapycenter/AppInitializer.java
@@ -1,4 +1,24 @@
 package lk.ijse.therapycenter;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    @Override
+    public void start(Stage stage)
+            throws Exception {
+
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml")))
+        );
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module TherapyCenter {
     requires org.hibernate.orm.core;
     requires java.naming;
     requires jbcrypt;
+    requires javafx.graphics;
+    requires javafx.fxml;
 
     opens lk.ijse.therapycenter.entity to org.hibernate.orm.core;
 }
