9267bedd90f0c03213fc3f3e3111fe6a865790bc
Oshan <git config --global user.email>
2026-05-10 16:43:17 +0530

    create therapy program entity

--- src/main/java/lk/ijse/therapycenter/entity/TherapyProgram.java
+++ src/main/java/lk/ijse/therapycenter/entity/TherapyProgram.java
@@ -1,4 +1,23 @@
 package lk.ijse.therapycenter.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class TherapyProgram {
+
+    @Id
+    private String programId;
+
+    private String name;
+    private String duration;
+    private double fee;
 }
