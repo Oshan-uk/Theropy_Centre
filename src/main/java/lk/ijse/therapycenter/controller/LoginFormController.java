fb925c71e18faf2aec0c54cd596b28064031a225
Oshan <git config --global user.email>
2026-05-10 18:56:02 +0530

    creat login form controller

--- src/main/java/lk/ijse/therapycenter/controller/LoginFormController.java
+++ src/main/java/lk/ijse/therapycenter/controller/LoginFormController.java
@@ -1,4 +1,23 @@
 package lk.ijse.therapycenter.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private CheckBox chkShowPassword;
+
+    @FXML
+    private Button btnLogin;
+
 }
