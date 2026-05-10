5497cd0ca7bb1754674a7cbd16bf72ebfc755c77
Oshan <git config --global user.email>
2026-05-10 17:36:01 +0530

    create bcrypt util

---
+++ src/main/java/lk/ijse/therapycenter/util/BCryptUtil.java
@@ -0,0 +1,14 @@
+package lk.ijse.therapycenter.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class BCryptUtil {
+
+    public static String encrypt(String password){
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean check(String password, String hash){
+        return BCrypt.checkpw(password, hash);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module TherapyCenter {
     requires static lombok;
     requires org.hibernate.orm.core;
     requires java.naming;
+    requires jbcrypt;
 
     opens lk.ijse.therapycenter.entity to org.hibernate.orm.core;
 }
