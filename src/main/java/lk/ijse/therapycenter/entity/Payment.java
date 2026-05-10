703b221d4c7a16a9a2c144bd799784483b07fdfe
Oshan <git config --global user.email>
2026-05-10 16:45:01 +0530

    create payment entity

--- src/main/java/lk/ijse/therapycenter/entity/Payment.java
+++ src/main/java/lk/ijse/therapycenter/entity/Payment.java
@@ -1,4 +1,23 @@
 package lk.ijse.therapycenter.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Payment {
+
+    @Id
+    private String paymentId;
+
+    private double amount;
+
+    private String paymentDate;
 }
