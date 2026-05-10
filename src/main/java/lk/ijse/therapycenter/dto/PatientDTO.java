6a960331f6a24077457d740e3cb7525755d2feb6
Oshan <git config --global user.email>
2026-05-10 11:06:44 +0530

    create patient dto

---
+++ src/main/java/lk/ijse/therapycenter/dto/PatientDTO.java
@@ -0,0 +1,11 @@
+package lk.ijse.therapycenter.dto;
+
+public class PatientDTO {
+
+    private String patientId;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+
+}
