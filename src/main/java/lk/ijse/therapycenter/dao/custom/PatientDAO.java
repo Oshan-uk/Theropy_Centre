615177d94911c641bc6eb0716105810bd13464ec
Oshan <git config --global user.email>
2026-05-11 09:54:21 +0530

    create patient dao

--- src/main/java/lk/ijse/therapycenter/dao/custom/PatientDAO.java
+++ src/main/java/lk/ijse/therapycenter/dao/custom/PatientDAO.java
@@ -2,6 +2,14 @@ package lk.ijse.therapycenter.dao.custom;
 
 import lk.ijse.therapycenter.dao.CrudDAO;
 import lk.ijse.therapycenter.entity.Patient;
+import java.util.List;
 
-public interface PatientDAO extends CrudDAO<Patient> {
+public interface PatientDAO extends CrudDAO<Patient, Integer> {
+    List<Patient> searchByName(String keyword);
+
+    List<Patient> findPatientsEnrolledInAllPrograms();
+
+    Patient findPatientWithPrograms(int patientId);
+
+    boolean existsByEmail(String email);
 }
