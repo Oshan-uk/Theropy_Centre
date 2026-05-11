40bdd51dedc835f24bae5c596f54fea4b64b2cf2
Oshan <git config --global user.email>
2026-05-11 09:48:07 +0530

    update user dao

--- src/main/java/lk/ijse/therapycenter/dao/custom/UserDAO.java
+++ src/main/java/lk/ijse/therapycenter/dao/custom/UserDAO.java
@@ -1,8 +1,12 @@
 package lk.ijse.therapycenter.dao.custom;
 
+import lk.ijse.therapycenter.dao.CrudDAO;
 import lk.ijse.therapycenter.entity.User;
 
-public interface UserDAO {
+public interface UserDAO extends CrudDAO<User, Integer> {
+    User findByUsername(String username);
 
-    User searchByUsername(String username);
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
